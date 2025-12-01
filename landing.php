6d2b12b249c0a6c77d6cb02d4eec1f96adfd9ceb
vfvb2023-2127-11806-cmd <vfvb2023-2127-11806@bicol-u.edu.ph>
2025-12-01 13:43:32 +0800

    Add files via upload

---
+++ landing.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUALITEES | Landing Page</title>
+    <link rel="stylesheet" href="./media/landing.css">
+    <link rel="icon" href="./media/icon.png" type="image/png">
+</head>
+
+<body>
+
+    <div class="container">
+
+        <!--left content icon (yung logo)-->
+        <div class="icon-box">
+            <img src="./media/icon0.png" alt="Logo Icon">
+        </div>
+
+        <!--right content (yung log in and sign up)-->
+        <div class="content">
+            <h1>QUALITEES</h1>
+            <h2>Join today.</h2>
+            <br>
+            <br>
+
+            <a href="./register.php"><button class="btn primary">Create Account</button></a>
+            <br>
+            <p class="small-text">Already have an account?</p>
+            <a href="./login.php"><button class="btn outline">Log In</button></a>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
